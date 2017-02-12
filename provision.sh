1f10675f621f1dbe4b556bbbd0ad2a47c1add1f5
gomesuit <gomesuit@gmail.com>
2017-02-12 22:43:12 +0900

    nginx

--- provision.sh
+++ provision.sh
@@ -21,7 +21,6 @@ sudo yum install -y lua-devel ncurses-devel git
 curl https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh | sh
 
 # setting vim
-#mkdir ~/.vim/indent
 ln -s /vagrant/settings/vimrc ~/.vimrc
 
 # install nodejs
@@ -35,6 +34,29 @@ nodebrew use v6.9.5
 sudo yum install -y bash-completion
 echo '[[ $PS1 && -f /usr/share/bash-completion/bash_completion ]] && source /usr/share/bash-completion/bash_completion' >> ~/.bash_profile
 
+# install rbenv
+sudo yum install -y git gcc bzip2 openssl-devel readline-devel zlib-devel
+git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
+echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bash_profile
+echo 'eval "$(rbenv init -)"' >> ~/.bash_profile
+source ~/.bash_profile
+git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
+
+# install ruby
+rbenv install 2.2.3
+rbenv rehash
+rbenv global 2.2.3
+
+# install sass
+gem install sass
+
+# install nginx
+sudo yum install -y epel-release
+sudo yum install -y nginx
+systemctl enable nginx
+ln -sf /vagrant/settings/nginx.conf /etc/nginx/nginx.conf
+systemctl start nginx
 
 # git clone git@github.com:gomesuit/larning-frontend.git
+
 
