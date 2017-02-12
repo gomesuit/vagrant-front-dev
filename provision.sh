7d421e2a86cce90a4ba884f9c8f614e0b3ee6174
gomesuit <gomesuit@gmail.com>
2017-02-12 19:01:56 +0900

    first commit

---
+++ provision.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+set -e
+
+# install vim
+sudo yum install -y lua-devel ncurses-devel git gcc
+git clone https://github.com/vim/vim.git
+cd vim
+./configure \
+  --prefix=/usr/local \
+  --with-features=huge \
+  --enable-multibyte \
+  --enable-luainterp \
+  --enable-cscope \
+  --enable-fail-if-missing
+make
+sudo make install
+cd
+
+# install NeoBundle
+sudo yum install -y lua-devel ncurses-devel git
+curl https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh | sh
+
+# setting vim
+#mkdir ~/.vim/indent
+#ln -s /vagrant/setting/ruby.vim ~/.vim/indent/ruby.vim
+#ln -s /vagrant/setting/vimrc ~/.vimrc
+
+# install nodejs
+curl -L git.io/nodebrew | perl - setup
+echo 'export PATH=$HOME/.nodebrew/current/bin:$PATH' >> .bash_profile
+source ~/.bash_profile
+nodebrew install-binary v6.9.5
+nodebrew use v6.9.5
+
+# install bash-completion
+sudo yum install -y bash-completion
+echo '[[ $PS1 && -f /usr/share/bash-completion/bash_completion ]] && source /usr/share/bash-completion/bash_completion' >> ~/.bash_profile
+
